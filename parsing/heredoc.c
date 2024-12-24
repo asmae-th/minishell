d3d80668ee06476252e37d3b5bcaafcbaec4609e
Asmae Tahtouh <atahtouh@e1r1p4.1337.ma>
2024-12-24 13:33:44 +0100

    norminete && expand handler

--- parsing/heredoc.c
+++ parsing/heredoc.c
@@ -3,202 +3,168 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asmae <asmae@student.42.fr>                +#+  +:+       +#+        */
+/*   By: atahtouh <atahtouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 12:41:46 by asmae             #+#    #+#             */
-/*   Updated: 2024/12/23 11:25:11 by asmae            ###   ########.fr       */
+/*   Updated: 2024/12/24 11:04:58 by atahtouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/parsing.h"
 
-void signal_herdoc_handler(int heredoc)
+void	signal_herdoc_handler(int heredoc)
 {
 	if (heredoc == SIGINT)
 	{
 		ft_setter(1, 1);
 		if (dup2(STDERR_FILENO, 0) == -1)
-        {
-            write(STDERR_FILENO, strerror(errno), ft_strlen(strerror(errno)));
-	        write(1, "\n", 1);
-        }
+		{
+			write(STDERR_FILENO, strerror(errno), ft_strlen(strerror(errno)));
+			write(1, "\n", 1);
+		}
 		write(1, "\n", 1);
 		close(STDIN_FILENO);
 	}
 }
 
-void ft_signal_heredoc(void)
+void	ft_signal_heredoc(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    sa.sa_handler = signal_herdoc_handler;
-    sa.sa_flags = SA_RESTART;
-    sigemptyset(&sa.sa_mask);
-
-    sigaction(SIGINT, &sa, NULL);
-    signal(SIGQUIT, SIG_IGN);
+	sa.sa_handler = signal_herdoc_handler;
+	sa.sa_flags = SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
 }
 
-
-char *get_env_value(char *var_name, t_env **env)
+char	*get_env_value(char *var_name, t_env **env)
 {
-    t_env *current = *env;
+	t_env	*current;
 
-    while (current->next)
-    {
-        // printf("chaeeek2222 ::: \n");
-        if (ft_strcmp(current->var, var_name) == 0)
-            return ft_strdup(current->val);
-        current = current->next;
-    }
-    return ft_strdup("");
+	current = *env;
+	while (current->next)
+	{
+		if (ft_strcmp(current->var, var_name) == 0)
+			return (ft_strdup(current->val));
+		current = current->next;
+	}
+	return (ft_strdup(""));
 }
 
-char *heredoc_expand(char **line, t_env **env)
+char	*heredoc_expand(char **line, t_env **env)
 {
-    char *result = ft_strdup("");
-    char *tmp = *line;
-    char *start, *var_name, *var_value;
-    int i = 0, j;
+	char	*result;
+	char	*tmp;
+	char	*start;
+	char	*var_name;
+	char	*var_value;
+	int		i;
+	int		j;
 
-    while (tmp && tmp[i])
-    {
-        if (tmp[i] == '$')
-        {
-            result = ft_strjoin(result, ft_substr(tmp, 0, i));
-            tmp += i + 1;
-            j = 0;
-            while (tmp[j] && (ft_isalnum(tmp[j]) || tmp[j] == '_'))
-                j++;
-            var_name = ft_substr(tmp, 0, j);
-            tmp += j;
-            var_value = get_env_value(var_name, env);
-            free(var_name);
-            result = ft_strjoin(result, var_value);
-            free(var_value);
-        }
-        else
-        {
-            start = tmp;
-            while (tmp[i] && tmp[i] != '$')
-                i++;
-            result = ft_strjoin(result, ft_substr(start, 0, i));
-            tmp += i;
-            i = 0;
-        }
-    }
-    free(*line);
-    *line = result;
-    return result;
+	i = 0;
+	result = ft_strdup("");
+	tmp = *line;
+	while (tmp && tmp[i])
+	{
+		if (tmp[i] == '$')
+		{
+			result = ft_strjoin(result, ft_substr(tmp, 0, i));
+			tmp += i + 1;
+			j = 0;
+			while (tmp[j] && (ft_isalnum(tmp[j]) || tmp[j] == '_'))
+				j++;
+			var_name = ft_substr(tmp, 0, j);
+			tmp += j;
+			var_value = get_env_value(var_name, env);
+			free(var_name);
+			result = ft_strjoin(result, var_value);
+			free(var_value);
+		}
+		else
+		{
+			start = tmp;
+			while (tmp[i] && tmp[i] != '$')
+				i++;
+			result = ft_strjoin(result, ft_substr(start, 0, i));
+			tmp += i;
+			i = 0;
+		}
+	}
+	free(*line);
+	*line = result;
+	return (result);
 }
 
-// int ft_check_herdoc_quote(tmp_cmd **cmd)
-// {
-//     tmp_cmd *tmp;
-
-//     tmp = *cmd;
-
-//     while (tmp)
-//     {
-//         if(tmp->type == HERE_DOC 
-//         && (tmp->next->state == IN_DQUOT || tmp->next->state == IN_SQUOT
-//         || tmp->next->next->state == IN_DQUOT || tmp->next->next->state == IN_SQUOT))
-//         {
-//             printf("helloo\n");
-//             return (1);
-//             // break;
-//         }
-//         tmp = tmp->next;
-//     }
-//     return (0);
-// }
-
-int ft_check_herdoc_quote(tmp_cmd **cmd)
+int	ft_check_herdoc_quote(t_tmp_cmd **cmd)
 {
-    tmp_cmd *tmp;
+	t_tmp_cmd	*tmp;
 
-    tmp = *cmd;
-
-    while (tmp)
-    {
-        // Vérifie si le heredoc est suivi par un token avec des quotes
-        if (tmp->type == HERE_DOC 
-            && (tmp->next 
-                && (tmp->next->state == IN_DQUOT || tmp->next->state == IN_SQUOT)))
-        {
-            printf("helloo\n");
-
-            return (1); // Indique qu'il y a une quote dans le délimiteur
-        }
-        tmp = tmp->next;
-    }
-    return (0); // Pas de quote détectée
+	tmp = *cmd;
+	while (tmp)
+	{
+		if (tmp->type == HERE_DOC && (tmp->next
+				&& (tmp->next->state == IN_DQUOT
+					|| tmp->next->state == IN_SQUOT)))
+		{
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
 }
 
-
-char *heredoc(char *file,int new_name, t_token_state state, t_env **env, t_herdoc valid)
+char	*heredoc(char *file, int new_name, t_env **env, t_herdoc valid)
 {
-	char *file_name;
-	char *redline;
-	int fd_heredoc;
-    int line;
+	char	*file_name;
+	char	*redline;
+	int		fd_heredoc;
+	int		line;
 
-    (void)state;
-    ft_signal_heredoc();
+	ft_signal_heredoc();
 	file_name = ft_strjoin("herdoc ", ft_itoa(new_name));
 	fd_heredoc = open(file_name, O_RDWR | O_CREAT, 0666);
-	line = 0; 
-	 while (1)
-	 {
+	line = 0;
+	while (1)
+	{
 		redline = readline("HEREDOC> ");
-        line++;
-        if(!redline)
-            printf("warning: here-document at line %d delimited by end-of-file (wanted `%s')\n",line, file);
-		if(!redline || ft_strcmp(redline, file) == 0)
+		line++;
+		if (!redline)
+			printf("warning: here-document at line %d\
+				delimited by end-of-file (wanted `%s')\n", line, file);
+		if (!redline || ft_strcmp(redline, file) == 0)
 		{
 			free(redline);
-			break;
+			break ;
 		}
-		if(ft_indice(redline, '$') != -1)
-        {
-            if(valid == FALS )
-			    redline = heredoc_expand(&redline, env);
-                
-            // if(ft_check_herdoc_quote(cmd) == 0)
-            // {
-            //     write(fd_heredoc, redline, ft_strlen(redline));
-		    //     write(fd_heredoc, "\n", 1);
-            //     continue;
-            // }
-            // else
-            // printf("chaeeek ::: %d\n",state);
-            
-        }
+		if (ft_indice(redline, '$') != -1)
+		{
+			if (valid == FALS)
+				redline = heredoc_expand(&redline, env);
+		}
 		write(fd_heredoc, redline, ft_strlen(redline));
 		write(fd_heredoc, "\n", 1);
 		free(redline);
 		redline = NULL;
-	 }
-	 return(file_name);
+	}
+	return (file_name);
 }
 
-void    ft_here_doc(tmp_cmd **cmd, t_env **env)
+void	ft_here_doc(t_tmp_cmd **cmd, t_env **env)
 {
-	tmp_cmd *current;
-	int     new_name;
+	t_tmp_cmd	*current;
+	int			new_name;
 
 	current = *cmd;
-    new_name = 0;
-	// int check = ft_check_herdoc_quote(cmd);
-	// 	printf("return :: %d\n",check);
+	new_name = 0;
 	while (current)
 	{
-		if(current->f_type == HEREDOC_FILE)
+		if (current->f_type == HEREDOC_FILE)
 		{
-			current->value = heredoc(current->value,new_name, current->state, env, current->valid);
+			current->value = heredoc(current->value, new_name,
+					env, current->valid);
 			new_name++;
 		}
 		current = current->next;
 	}
-	
 }
--- parsing/more_analyse.c
+++ parsing/more_analyse.c
@@ -3,491 +3,403 @@
 /*                                                        :::      ::::::::   */
 /*   more_analyse.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asmae <asmae@student.42.fr>                +#+  +:+       +#+        */
+/*   By: atahtouh <atahtouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 09:00:09 by asmae             #+#    #+#             */
-/*   Updated: 2024/12/23 14:53:46 by asmae            ###   ########.fr       */
+/*   Updated: 2024/12/24 11:37:07 by atahtouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/parsing.h"
 
-int search_quote(char *value)
-{
-	int i;
-
-	i = 0;
-	if (!value)
-		return (0);
-	while (value[i])
-	{
-		if(value[i] == '\"' || value[i] == '\'')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int len_value(char *value, t_token_state state)
-{
-	int i;
-	int len;
-	
-	i = 0;
-	len = 0;
-	if(state == IN_DQUOT)
-	{	
-		while (value[i])
-		{
-		if(value[i] != '\"')
-				len++;
-			i++;
-		}
-	}
-	else if(state == IN_SQUOT)
-	{	
-		while (value[i])
-		{
-		if(value[i] != '\'')
-				len++;
-			i++;
-		}
-	}
-	return (len);
-}
-char    *supr_quote(char *value, t_token_state state)
-{
-	int i;
-	int	j;
-	char *ptr;
-
-	ptr = (char *)malloc((len_value(value, state) + 1) * sizeof(char));
-	if(!ptr)
-		return (NULL);
-	i = 0;
-	j = 0;
-	if(state == IN_DQUOT)
-	{
-		while (value[i])
-		{
-			if(value[i] != '\"')
-			{
-				ptr[j] = value[i];
-				j++;
-			}
-			i++;
-		}
-	}
-	else if(state == IN_SQUOT)
-	{
-		while (value[i])
-		{
-			if(value[i] != '\'')
-			{
-				ptr[j] = value[i];
-				j++;
-			}
-			i++;
-		}
-	}
-	ptr[j] = '\0';
-	free(value);
-	return (ptr);
-}
-
-// void	ft_pos_init(t_position **pos, t_token **token)
+// int	search_quote(char *value)
 // {
-// 	t_token *current;
-// 	int	nb_node;
+// 	int	i;
 
-// 	nb_node = 0;
-// 	if(!pos)
-// 		return;
-// 	(*pos)->start = 0;
-// 	(*pos)->end = 0;
-// 	(*pos)->flag = 0;
-// 	current = (*token);
-// 	while (current)
-// 	{
-// 		current->index = nb_node;
-// 		nb_node++;
-// 		current = current->next;
-// 	}	
-// 	(*pos)->node_member = nb_node;
-// }
-
-// int	ft_state_check(t_token *token)
-// {
-// 	if(token->type != EXIT_STATUS &&
-// 	(token->state == IN_DQUOT || token->state == IN_SQUOT || token->type == CMD))
-// 		return (1);
-// 	else
+// 	i = 0;
+// 	if (!value)
 // 		return (0);
+// 	while (value[i])
+// 	{
+// 		if (value[i] == '\"' || value[i] == '\'')
+// 			return (1);
+// 		i++;
+// 	}
+// 	return (0);
 // }
 
-// t_position	*ft_get_pos(t_token **token)
+// int	len_value(char *value, t_token_state state)
 // {
-// 	t_token	*current;
-// 	t_position *pos;
-	
-// 	pos = (t_position *)malloc(sizeof(t_position));
-// 	ft_pos_init(&pos, token);
-// 	current = *token;
-// 	while (current && current->next)
+// 	int	i;
+// 	int	len;
+
+// 	i = 0;
+// 	len = 0;
+// 	if (state == IN_DQUOT)
 // 	{
-// 		while (current && current->next && current->index < pos->node_member
-// 				&& ft_state_check(current) && ft_state_check(current->next))
+// 		while (value[i])
 // 		{
-// 			if(pos->flag == 0)
-// 				pos->start = current->index;
-// 			pos->flag = 1;
-// 			current = current->next;
-// 			if(pos->flag)
-// 			{
-// 				pos->end = current->index;
-// 				return (pos);
-// 			}
-// 		}	
-// 		current = current->next;
+// 			if (value[i] != '\"')
+// 				len++;
+// 			i++;
+// 		}
 // 	}
-// 	return (pos);
+// 	else if (state == IN_SQUOT)
+// 	{
+// 		while (value[i])
+// 		{
+// 			if (value[i] != '\'')
+// 				len++;
+// 			i++;
+// 		}
+// 	}
+// 	return (len);
 // }
 
-// char	*ft_value(t_token *token, int end)
+// char	*supr_quote(char *value, t_token_state state)
 // {
-// 	char *ptr;
-	
-// 	ptr = ft_strndup(token->value, (int)ft_strlen(token->value));
-// 	while (token->index < end)
+// 	int		i;
+// 	int		j;
+// 	char	*ptr;
+
+// 	ptr = (char *)malloc((len_value(value, state) + 1) * sizeof(char));
+// 	if (!ptr)
+// 		return (NULL);
+// 	i = 0;
+// 	j = 0;
+// 	if (state == IN_DQUOT)
 // 	{
-// 		ptr = ft_strjoin(ptr, token->next->value);
-// 		token = token->next;
+// 		while (value[i])
+// 		{
+// 			if (value[i] != '\"')
+// 			{
+// 				ptr[j] = value[i];
+// 				j++;
+// 			}
+// 			i++;
+// 		}
 // 	}
+// 	else if (state == IN_SQUOT)
+// 	{
+// 		while (value[i])
+// 		{
+// 			if (value[i] != '\'')
+// 			{
+// 				ptr[j] = value[i];
+// 				j++;
+// 			}
+// 			i++;
+// 		}
+// 	}
+// 	ptr[j] = '\0';
+// 	free(value);
 // 	return (ptr);
 // }
 
-// t_token *new_clean_node(t_token **token, t_position *pos)
+// char	*assemble_data(t_token *begin, int end)
 // {
-// 	t_token	*current;
-// 	t_token *node;
+// 	char	*join;
 
-// 	current = *token;
-// 	while (current)
+// 	join = ft_strndup(begin->value, (int)ft_strlen(begin->value));
+// 	while (begin->index < end)
 // 	{
-// 		if(current->index == pos->start)
-// 		{
-// 			node = create_token(ft_value(current, pos->end), CMD, NORMAL);
-// 			node->index = pos->start;
-// 			node->prev = current->prev;
-// 		}
-// 		if(current->index == pos->end)
-// 			node->next = current->next;
-// 		current = current->next;
+// 		join = ft_strjoin(join, begin->next->value);
+// 		begin = begin->next;
 // 	}
-// 	return (node);
+// 	return (join);
 // }
 
-// void ft_free_start_end(t_token **start, t_token **end)
+// t_token	*data_assembler(t_token **tokens, t_position *position)
 // {
-// 	t_token *current;
-// 	t_token *tmp;
+// 	t_token	*cursur;
+// 	t_token	*new_node;
+
+// 	cursur = (*tokens);
+// 	while (cursur)
+// 	{
+// 		if (cursur->index == position->start)
+// 		{
+// 			new_node = create_token(assemble_data(cursur,
+// 						position->end), CMD, NORMAL);
+// 			new_node->index = position->start;
+// 			new_node->prev = cursur->prev;
+// 		}
+// 		if (cursur->index == position->end)
+// 			new_node->next = cursur->next;
+// 		cursur = cursur->next;
+// 	}
+// 	return (new_node);
+// }
+
+// void	free_region(t_token **start, t_token **end)
+// {
+// 	t_token	*current;
+// 	t_token	*next;
 
 // 	current = *start;
-// 	while (current)
+// 	while (current != NULL)
 // 	{
-// 		tmp = current->next;
+// 		next = current->next;
 // 		free(current->value);
 // 		free(current);
-// 		current = tmp;
-// 		if(current == *end)
+// 		current = next;
+// 		if (current == (*end))
 // 		{
 // 			free(current->value);
 // 			free(current);
-// 			break;
+// 			break ;
 // 		}
 // 	}
-	
-// }
-// void	add_clean_node(t_token **token , int i_start, int i_end, t_token *clean_node)
-// {
-// 	t_token *start;
-// 	t_token *end;
-// 	int i;
-	
-// 	start = *token;
-// 	end = *token;
-// 	i = -1;
-// 	while (++i < i_start && start != NULL)
-// 		start = start->next;
-// 	i = -1;
-// 	while (++i < i_end && end != NULL)
-// 		end = end->next;
-// 	if(end->prev == NULL)
-// 		*token = clean_node;
-// 	else
-// 		start->prev->next = clean_node;
-// 	if(end->next != NULL)
-// 		end->next->prev = clean_node;
-// 	clean_node->prev = start->prev;
-// 	clean_node->next = end->next;
-// 	ft_free_start_end(&start, &end);
-// 		// printf("freeeee");
 // }
 
-// void	ft_clean(t_token **token)
+// int	true_state(t_token *current)
 // {
-// 	t_token *current;
-// 	t_position *pos;
-	
+// 	return (current->type != EXIT_STATUS && \
+// 		(current->state == IN_SQUOT || \
+// 		current->state == IN_DQUOT || current->type == CMD));
+// }
+
+// int	affect_index(t_token **token)
+// {
+// 	t_token	*cursur;
+// 	int		node_member;
+
+// 	node_member = 0;
+// 	cursur = (*token);
+// 	while (cursur)
+// 	{
+// 		cursur->index = node_member;
+// 		node_member++;
+// 		cursur = cursur->next;
+// 	}
+// 	return (node_member);
+// }
+
+// void	init_holder(t_position **holder, t_token **tokens)
+// {
+// 	if (*holder == NULL)
+// 		return ;
+// 	(*holder)->start = 0;
+// 	(*holder)->end = 0;
+// 	(*holder)->flag = 0;
+// 	(*holder)->node_member = affect_index(tokens);
+// }
+
+// t_position	*index_getter(t_token **tokens)
+// {
+// 	t_position	*holder;
+// 	t_token		*current;
+
+// 	current = (*tokens);
+// 	holder = (t_position *)malloc(sizeof(t_position));
+// 	init_holder(&holder, tokens);
+// 	while (current && current->next)
+// 	{
+// 		while (current && current->next && current->index < holder->node_member \
+// 			&& true_state(current) && true_state(current->next))
+// 		{
+// 			if (holder->flag == 0)
+// 				holder->start = current->index;
+// 			holder->flag = 1;
+// 			current = current->next;
+// 		}
+// 		if (holder->flag)
+// 		{
+// 			holder->end = current->index;
+// 			return (holder);
+// 		}
+// 		current = current->next;
+// 	}
+// 	return (holder);
+// }
+
+// void	ft_clean(t_token **tokens)
+// {
+// 	t_token		*cursur;
+// 	t_position	*to_extract;
+// 	t_token		*node;
+
+// 	cursur = (*tokens);
+// 	while (cursur)
+// 	{
+// 		to_extract = index_getter(tokens);
+// 		if (to_extract->start != to_extract->end)
+// 		{
+// 			node = data_assembler(tokens, to_extract);
+// 			ft_free_token(tokens);
+// 			add_token(tokens, node);
+// 			cursur = (*tokens);
+// 		}
+// 		cursur = cursur->next;
+// 		free(to_extract);
+// 	}
+// }
+
+// ////////////////////////////////////////////////////////////
+// int	ft_segfault(t_token **token)
+// {
+// 	t_token	*current;
+
 // 	current = *token;
 // 	while (current)
 // 	{
-// 		pos = ft_get_pos(token);
-// 		if(pos->start != pos->end)
+// 		if (current->type == SYNTAX_ERROR)
+// 			return (0);
+// 		current = current->next;
+// 	}
+// 	return (1);
+// }
+
+// void	more_analyse(t_token **token)
+// {
+// 	t_token	*current;
+
+// 	if (!token || !*token)
+// 		return ;
+// 	current = *token;
+// 	while (current)
+// 	{
+// 		if (search_quote(current->value)
+// 			&& (current->state == IN_DQUOT || current->state == IN_SQUOT))
 // 		{
-// 			add_clean_node(token, pos->start, pos->end, new_clean_node(token, pos));
-// 			current = *token;
+// 			current->value = supr_quote(current->value, current->state);
+// 			current->state = NORMAL;
 // 		}
 // 		current = current->next;
-// 		free(pos);
 // 	}
+// 	if (ft_segfault(token) == 1 && (*token)->node_member != 1)
+// 		ft_clean(token);
 // }
 
-
-
-
-
-
-
-
-
-
-char	*assemble_data(t_token *begin, int end)
+char *remove_quotes(char *value, t_token_state state)
 {
-	char	*join;
+    int len = 0, i = 0, j = 0;
+    char *result;
 
-	join = ft_strndup(begin->value, (int)ft_strlen(begin->value));
-	while (begin->index < end)
-	{
-		join = ft_strjoin(join, begin->next->value);
-		begin = begin->next;
-	}
-	return (join);
-}
+    if (!value)
+        return NULL;
 
-// t_token	*build_new_token_node(char *token_data, enum e_token_type type,
-// 		enum e_token_state state)
-// {
-// 	t_token	*token;
+    // Calculate the length of the string without quotes
+    while (value[i]) {
+        if ((state == IN_DQUOT && value[i] != '"') || 
+            (state == IN_SQUOT && value[i] != '\'')) {
+            len++;
+        }
+        i++;
+    }
 
-// 	token = malloc(sizeof(t_token));
-// 	if (!token)
-// 		return (NULL);
-// 	token->value = token_data;
-// 	token->type = type;
-// 	token->state = state;
-// 	token->index = 0;
-// 	token->node_member = 0;
-// 	token->next = NULL;
-// 	token->prev = NULL;
-// 	return (token);
-// }
+    result = malloc(len + 1);
+    if (!result)
+        return NULL;
 
-t_token	*data_assembler(t_token **tokens, t_position *position)
-{
-	t_token	*cursur;
-	t_token	*new_node;
-
-	cursur = (*tokens);
-	while (cursur)
-	{
-		if (cursur->index == position->start)
-		{
-			new_node = create_token(assemble_data(cursur,
-						position->end), CMD, NORMAL);
-			new_node->index = position->start;
-			new_node->prev = cursur->prev;
-		}
-		if (cursur->index == position->end)
-			new_node->next = cursur->next;
-		cursur = cursur->next;
-	}
-	return (new_node);
-}
-
-void	free_region(t_token **start, t_token **end)
-{
-	t_token	*current;
-	t_token	*next;
-
-	current = *start;
-	while (current != NULL)
-	{
-		next = current->next;
-		free(current->value);
-		free(current);
-		current = next;
-		if (current == (*end))
-		{
-			free(current->value);
-			free(current);
-			break ;
-		}
-	}
-}
-
-void	addclean_token(t_token **head, int start_index, int end_index, t_token *clean_node)
-{
-	t_token	*start;
-	t_token	*end;
-	int				i;
-
-	start = *head;
-	end = *head;
-	i = -1;
-	while (++i < start_index && start != NULL)
-		start = start->next;
-	i = -1;
-	while (++i < end_index && end != NULL)
-		end = end->next;
-	if (start->prev == NULL)
-		*head = clean_node;
-	else
-		start->prev->next = clean_node;
-	if (end->next != NULL)
-		end->next->prev = clean_node;
-	clean_node->prev = start->prev;
-	clean_node->next = end->next;
-		t_token	*tmp = *head;
-			while (tmp)
-			{
-					printf("%s\n",tmp->value);
-					tmp = tmp->next;
-			}
-	// free_region(&start, &end);
-}
-
-int	true_state(t_token *current)
-{
-	return (current->type != EXIT_STATUS && \
-		(current->state == IN_SQUOT || \
-		current->state == IN_DQUOT || current->type == CMD));
-}
-
-int	affect_index(t_token **token)
-{
-	t_token	*cursur;
-	int				node_member;
-
-	node_member = 0;
-	cursur = (*token);
-	while (cursur)
-	{
-		cursur->index = node_member;
-		node_member++;
-		cursur = cursur->next;
-	}
-	// printf("%d\n",node_member);<
-	return (node_member);
-}
-
-void	init_holder(t_position **holder, t_token **tokens)
-{
-	if (*holder == NULL)
-		return ;
-	(*holder)->start = 0;
-	(*holder)->end = 0;
-	(*holder)->flag = 0;
-	(*holder)->node_member = affect_index(tokens);
-}
-
-t_position	*index_getter(t_token **tokens)
-{
-	t_position		*holder;
-	t_token	*current;
-
-	current = (*tokens);
-	holder = (t_position *)malloc(sizeof(t_position));
-	init_holder(&holder, tokens);
-	while (current && current->next)
-	{
-		while (current && current->next && current->index < holder->node_member \
-			&& true_state(current) && true_state(current->next))
-		{
-			if (holder->flag == 0)
-				holder->start = current->index;
-			holder->flag = 1;
-			current = current->next;
-		}
-		if (holder->flag)
-		{
-			holder->end = current->index;
-			return (holder);
-		}
-		current = current->next;
-	}
-	return (holder);
-}
-
-void	ft_clean(t_token **tokens)
-{
-	t_token	*cursur;
-	t_position		*to_extract;
-	
-
-	cursur = (*tokens);
-	while (cursur)
-	{
-		to_extract = index_getter(tokens);
-		if (to_extract->start != to_extract->end)
-		{
-			t_token *node;
-			node = data_assembler(tokens, to_extract);
-			ft_free_token(tokens);
-			add_token(tokens, node);
-			// addclean_token(tokens, to_extract->start, to_extract->end, node);
-			cursur = (*tokens);
-		}
-		cursur = cursur->next;
-		free(to_extract);
-	}
-	
-}
-
-////////////////////////////////////////////////////////////
-int	ft_segfault(t_token **token)
-{
-	t_token *current;
-
-	current = *token;
-	while (current)
-	{
-		if(current->type == SYNTAX_ERROR)
-			return (0);
-		current = current->next;
-	}
-	return (1);
-}
-
-void    more_analyse(t_token **token)
-{
-	t_token *current;
-
-	if (!token || !*token)
-    return;
-	current = *token;
-	while (current)
-	{
-		if (search_quote(current->value) && (current->state == IN_DQUOT || current->state == IN_SQUOT))
-		{
-			current->value = supr_quote(current->value, current->state);
-			current->state = NORMAL;
-		}
-		current = current->next;
-	}
-	if(ft_segfault(token) == 1)
-		ft_clean(token);
-	
+    i = 0;
+    while (value[i]) {
+        if ((state == IN_DQUOT && value[i] != '"') || 
+            (state == IN_SQUOT && value[i] != '\'')) {
+            result[j++] = value[i];
+        }
+        i++;
+    }
+    result[j] = '\0';
+    free(value);
+    return result;
 }
 
 
+void process_quotes(t_token **tokens)
+{
+    t_token *current = *tokens;
+
+    while (current) {
+        if (current->state == IN_DQUOT || current->state == IN_SQUOT) {
+            current->value = remove_quotes(current->value, current->state);
+            current->state = NORMAL;
+        }
+        current = current->next;
+    }
+}
+
+
+t_token *merge_tokens(t_token *start, t_token *end)
+{
+    char *merged_value = ft_strdup(start->value);
+    t_token *current = start->next;
+
+    while (current && current != end->next) {
+        char *temp = ft_strjoin(merged_value, current->value);
+        free(merged_value);
+        merged_value = temp;
+        current = current->next;
+    }
+
+    t_token *new_token = create_token(merged_value, CMD, NORMAL);
+    new_token->index = start->index;
+
+    free(merged_value);
+    return new_token;
+}
+
+void replace_tokens(t_token **tokens, t_token *start, t_token *end, t_token *new_token)
+{
+    t_token *prev = start->prev;
+    t_token *next = end->next;
+
+    // Relier le nouveau token à la liste
+    if (prev) {
+        prev->next = new_token;
+        new_token->prev = prev;
+    } else {
+        // Si le début de la liste est remplacé
+        *tokens = new_token;
+    }
+
+    if (next) {
+        next->prev = new_token;
+        new_token->next = next;
+    }
+
+    // Libérer les anciens tokens
+    t_token *current = start;
+    while (current != next) {
+        t_token *temp = current;
+        current = current->next;
+        free(temp->value);
+        free(temp);
+    }
+}
+
+
+void clean_tokens(t_token **tokens)
+{
+    t_token *current = *tokens;
+    t_token *start = NULL, *end = NULL;
+
+    while (current) {
+        if (current->state == IN_DQUOT || current->state == IN_SQUOT) {
+            if (!start)
+                start = current;
+            end = current;
+        } else if (start && end) {
+            t_token *new_token = merge_tokens(start, end);
+            replace_tokens(tokens, start, end, new_token);
+            current = *tokens; // Restart from the beginning
+            start = end = NULL;
+        }
+        current = current->next;
+    }
+
+    if (start && end) {
+        t_token *new_token = merge_tokens(start, end);
+        replace_tokens(tokens, start, end, new_token);
+    }
+}
+
+
+void analyze_tokens(t_token **tokens)
+{
+    if (!tokens || !*tokens)
+        return;
+
+    process_quotes(tokens);
+    clean_tokens(tokens);
+}
